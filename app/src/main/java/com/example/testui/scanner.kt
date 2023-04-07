77265e47564016e2e8c391ceb66397a3fb6d2935
ArgusWrites <baylonfrancis4work@gmail.com>
2023-04-07 12:54:34 +0800

    Sera Test UI

---
+++ app/src/main/java/com/example/testui/scanner.kt
@@ -0,0 +1,12 @@
+package com.example.testui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class scanner : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scanner)
+    }
+} {
+}
